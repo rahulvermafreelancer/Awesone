4c3f5e89b05cf38cfa499110b40b99819ee950e5
Rahul Verma <rahulvermafreelancer@gmail.com>
2024-09-12 02:59:15 -0700

    added 1st sample

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hi I just completed my first React Native Assignment</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+// const styles = StyleSheet.create({});
+
+export default App;
